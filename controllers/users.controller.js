fe1d74721fb3a6c36594bf28d01505bb34db41e3
Abimbola Oluwatobiloba J <71207758+JCWORLD-SOFTWARE@users.noreply.github.com>
2022-09-25 00:46:06 +0100

    Add files via upload

---
+++ controllers/users.controller.js
@@ -0,0 +1,110 @@
+var Promise = require('promise');
+var config = require('./../config/config');
+var User = require('./../models/user');
+
+module.exports = {
+  createUser: function(req, res) {
+    User.create(req.body)
+      .then(function(result) {
+        return res.status(200).json({
+          message: 'success! created account for new user',
+          id: result.id
+        });
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+
+  changeName: function(req, res) {
+    User.updateName({ id: req.params.id, name: req.body.name })
+      .then(function(result) {
+        return res.status(200).json(result);
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+
+  changeEmail: function(req, res) {
+    User.updateEmail({ id: req.params.id, email: req.body.email })
+      .then(function(result) {
+        return res.status(200).json(result);
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+
+  changePassword: function(req, res) {
+    User.updatePassword({ id: req.params.id, password: req.body.password })
+      .then(function(result) {
+        return res.status(200).json(result);
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+
+  deleteUser: function(req, res) {
+    User.delete({ id: req.params.id })
+      .then(function(result) {
+        return res.status(200).json({
+          message: 'deleted user with id: ' + result.id
+        });
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+
+  getOneUser: function(req, res) {
+    User.findOne({ id: req.params.id })
+      .then(function(result) {
+        delete result.last_login_attempt;
+        delete result.login_attempts;
+        return res.status(200).json(result);
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+
+  getSelfUser: function(req, res) {
+    User.findOne({ id: req.decoded.sub })
+      .then(function(result) {
+        delete result.last_login_attempt;
+        delete result.login_attempts;
+        return res.status(200).json(result);
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+
+  listUsers: function(req, res) {
+    User.findAll()
+      .then(function(result) {
+        return res.status(200).json(result);
+      })
+      .catch(function(err) {
+        return res.status(400).json({
+          message: err
+        });
+      });
+  },
+};
